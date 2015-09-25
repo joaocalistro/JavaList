bc6ab410a58357e045e6ef88f44418e7558ef357
Joao Calistro <jccalistro@gmail.com>
2015-09-25 14:07:05 -0300

    List1 ex7

---
+++ list1/Age.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+public class Age {
+  public static void main(String[] args) {
+    int years;
+    int months;
+    int days;
+    int ageInDays;
+    String dataInput;
+    dataInput = JOptionPane.showInputDialog ("Digite sua idade em anos:");
+    years = Integer.parseInt(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite quantos meses se passaram após seu aniversário:");
+    months = Integer.parseInt(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite quantos dias se passaram após o dia em que faz aniversário:");
+    days = Integer.parseInt(dataInput);
+    ageInDays = (years * 365) + (months * 30) + days;
+    JOptionPane.showMessageDialog(null, String.format("Sua idade em dias é de %d.", ageInDays));
+  }
+}
