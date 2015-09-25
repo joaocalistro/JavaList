d22e2e4f9f9044b81cde55d3439660500490f56e
Joao Calistro <jccalistro@gmail.com>
2015-09-25 10:43:11 -0300

    List1 ex1

---
+++ list1/CanPrice.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+public class CanPrice {
+  public static void main(String[] args) {
+    double aluminiumPricePerSqMeter;
+    double canRadius;
+    double canHigh;
+    double canArea;
+    double canPrice;
+    String radiusInput;
+    String highInput;
+    aluminiumPricePerSqMeter = 100.00;
+    radiusInput = JOptionPane.showInputDialog ("Digite o raio da lata em cm: ");
+    canRadius = Double.parseDouble(radiusInput);
+    highInput = JOptionPane.showInputDialog ("Digite a altura da lata em cm: ");
+    canHigh = Double.parseDouble(highInput);
+
+    canArea = (3.14 * ((canRadius*canRadius) * 2)) + (((2 * 3.14) * canRadius) * canHigh);
+    canPrice = canArea * (aluminiumPricePerSqMeter / 10000);
+    JOptionPane.showMessageDialog(null, String.format("Para a lata de área %.2fcm2 o custo será de R$%.2f", canArea, canPrice));
+  }
+}
