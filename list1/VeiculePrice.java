a00064db251e684c3bce0c8ddcbe89833eabfd3a
Joao Calistro <jccalistro@gmail.com>
2015-09-25 11:55:49 -0300

    List1 ex3

---
+++ list1/VeiculePrice.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class VeiculePrice {
+  public static void main(String[] args) {
+    double factoryPrice;
+    double customerPrice;
+    int dealerPercentage;
+    int tax;
+    String priceInput;
+    dealerPercentage = 12;
+    tax = 30;
+    priceInput = JOptionPane.showInputDialog ("Digite o custo de fábrica do veiculo:");
+    factoryPrice = Double.parseDouble(priceInput);
+    customerPrice = (factoryPrice * tax)/100;
+    customerPrice += (((customerPrice+factoryPrice) * dealerPercentage) / 100) + factoryPrice;
+    JOptionPane.showMessageDialog(null, String.format("O valor do veiculo para venda ao consumidor será de R$%.2f.", customerPrice));
+  }
+}
