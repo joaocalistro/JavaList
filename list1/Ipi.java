b7ee662249462ee57d835c4bcd0be9d9e4093743
Joao Calistro <jccalistro@gmail.com>
2015-09-27 18:00:59 -0300

    List1 ex9

---
+++ list1/Ipi.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+public class Ipi {
+  public static void main(String[] args) {
+    double ipi;
+    double product1;
+    double value1;
+    double quantity1;
+    double product2;
+    double value2;
+    double quantity2;
+    double result;
+    String dataInput;
+    dataInput = JOptionPane.showInputDialog ("Digite a porcentagem do IPI:");
+    ipi = Double.parseDouble(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite o código do produto 1:");
+    product1 = Double.parseDouble(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite a quantidade do produto 1:");
+    quantity1 = Double.parseDouble(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite o valor do produto 1:");
+    value1 = Double.parseDouble(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite o código do produto 2:");
+    product2 = Double.parseDouble(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite a quantidade do produto 2:");
+    quantity2 = Double.parseDouble(dataInput);
+    dataInput = JOptionPane.showInputDialog ("Digite o valor do produto 2:");
+    value2 = Double.parseDouble(dataInput);
+    result = (quantity1 * value1) + (quantity2 * value2) * ((ipi / 100) + 1);
+    JOptionPane.showMessageDialog(null, String.format("Produto 1:\nCódigo: %.0f\nQuantidade: %.0f\nValor: R$%.2f\n\nProduto 2:\nCódigo: %.0f\nQuantidade: %.0f\nValor: R$%.2f\n\nIPI: %.2f%%\n\nValor total a ser pago: R$%.2f", product1, quantity1, value1, product2, quantity2, value2, ipi, result));
+  }
+}
