91169120ac506a0cdf63a305b8b35795ef210eb0
Joao Calistro <jccalistro@gmail.com>
2015-09-27 17:34:22 -0300

    List1 ex8

---
+++ list1/FactoryEvent.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+public class FactoryEvent {
+  public static void main(String[] args) {
+    double seconds;
+    double hours;
+    double minutes;
+    String dataInput;
+    dataInput = JOptionPane.showInputDialog ("Digite o tempo em segundos:");
+    seconds = Double.parseDouble(dataInput);
+    hours = (int)(seconds / 3600);
+    seconds = seconds - (hours * 3600);
+    minutes = (int)(seconds / 60);
+    seconds = seconds - (minutes * 60);
+    JOptionPane.showMessageDialog(null, String.format("%.0f Hora(s), %.0f minuto(s) e %.0f segundo(s).", hours, minutes, seconds));
+  }
+}
