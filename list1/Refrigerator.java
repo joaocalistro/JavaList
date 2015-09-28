ed600c94c66c5bb638e20764bf9ec197337465cd
Joao Calistro <jccalistro@gmail.com>
2015-09-27 22:13:42 -0300

    List1 ex14

---
+++ list1/Refrigerator.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+public class Refrigerator {
+  public static void main(String[] args) {
+  String dataInput;
+  dataInput = JOptionPane.showInputDialog("Taxa de potência do refrigerador em watts:");
+  double refrigeratorPower = Double.parseDouble(dataInput);
+  dataInput = JOptionPane.showInputDialog("Tempo ligado em horas:");
+  double timeWorking = Double.parseDouble(dataInput);
+  dataInput = JOptionPane.showInputDialog("Valor do quilowatt/hora:");
+  double kilowattHourValue = Double.parseDouble(dataInput);
+  double energyExpended = refrigeratorPower * timeWorking;
+  double priceToPay = refrigeratorPower * kilowattHourValue;
+  JOptionPane.showMessageDialog(null, String.format("Energia total gasta: %.0f\n\nValor a ser pago: R$%.2f", energyExpended, priceToPay));
+  }
+}
+
