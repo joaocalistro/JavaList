6c20b8162f3cce095e546cb4fb1ae08178ab2790
Joao Calistro <jccalistro@gmail.com>
2015-09-27 21:22:04 -0300

    List1 ex13

---
+++ list1/Soccer.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+public class Soccer {
+  public static void main(String[] args) {
+  String dataInput;
+  dataInput = JOptionPane.showInputDialog("Público pagante:");
+  double audience = Double.parseDouble(dataInput);
+  double popular = ((audience * 10) / 100);
+  double popularIncome = (popular * 1);
+  double general = ((audience * 50) / 100);
+  double generalIncome = (general * 5);
+  double bleachers = ((audience * 30) / 100);
+  double bleachersIncome = (bleachers * 10);
+  double chairs = ((audience * 10) / 100);
+  double chairsIncome = (chairs * 20);
+  double income = popularIncome + generalIncome + bleachersIncome + chairsIncome;
+  JOptionPane.showMessageDialog(null, String.format("Renda do Jogo:\n\nPopular %.0f - R$%.2f\nGeral %.0f - R$%.2f\nArquibancada %.0f - R$%.2f\nCadeiras %.0f - R$%.2f\n\nRenda do jogo: R$%.2f", popular, popularIncome, general, generalIncome, bleachers, bleachersIncome, chairs, chairsIncome, income));
+  }
+}
+
